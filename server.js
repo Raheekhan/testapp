5c939b52ca1be0d4dd8e7446ce1314c9f104ca8e
Raheekhan <ibrahimkhan0894@gmail.com>
2018-03-01 13:07:14 -0500

    second commit

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var app = express();
 var http = require('http').Server(app);
 var router = express.Router();
 
-var port = 3000;
+// var port = 3000;
 var path = __dirname + '/';
+
+app.set('port', (process.env.PORT || 5000));
 
 router.get('/index', (req, res) => {
   res.sendFile(path + 'index.html');
@@ -14,6 +16,6 @@ router.get('/index', (req, res) => {
 
 app.use('/', router);
 
-http.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+http.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}`)
 });
